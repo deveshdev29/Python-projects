ed7f044ff2e101f7c9f7e832871d701471f26058
Devesh <deveshdev2909@gmail.com>
2023-09-07 20:29:32 +0530

    fixed bug

--- guess-the-number.py
+++ guess-the-number.py
@@ -4,7 +4,6 @@ lower_bound = int(input('Enter the Lower Bound : '))
 upper_bound = int(input('Enter the Upper Bound : '))
 
 random_int = random.randint(lower_bound,upper_bound)
-print(random_int)
 print('------You have 7 chance to guess------')
 count = 0
 for i in range(7):
@@ -13,8 +12,12 @@ for i in range(7):
 
     if guess < random_int:
         print('----Guess higher----')
+        if count == 7:
+            print("---------------------Sorry You Lost---------------------")
     elif guess > random_int:
         print('----Guess Lower----')
+        if count == 7:
+            print("---------------------Sorry You Lost---------------------")
     elif guess == random_int:
         print("----That's correct----")
         break
