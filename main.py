59f55195403057b8c6dee57f51e612b3c053550a
Devesh <deveshdev2909@gmail.com>
2023-10-06 10:45:20 +0530

    project

--- main.py
+++ main.py
@@ -1,54 +1,92 @@
-def printboard(xstate,ystate):
-    zero  = 'X' if xstate[0] else ('O' if ystate[0] else 0)
-    one  = 'X' if xstate[1] else ('O' if ystate[1] else 1)
-    two  = 'X' if xstate[2] else ('O' if ystate[2] else 2)
-    three  = 'X' if xstate[3] else ('O' if ystate[3] else 3)
-    four  = 'X' if xstate[4] else ('O' if ystate[4] else 4)
-    five  = 'X' if xstate[5] else ('O' if ystate[5] else 5)
-    six = 'X' if xstate[6] else ('O' if ystate[6] else 6)
-    seven  = 'X' if xstate[7] else ('O' if ystate[7] else 7)
-    eight= 'X' if xstate[8] else ('O' if ystate[8] else 8)
-    print(f'{zero} | {one} | {two}')
-    print(f'--|---|---')
-    print(f'{three} | {four} | {five}')
-    print(f'--|---|---')
-    print(f'{six} | {seven} | {eight}')
+import tkinter as tk  
+  
+root = tk.Tk()  
+root.resizable(True, True)  
+root.title("Tic Tac Toe")  
+  
+tk.Label(root, text="Tic Tac Toe", font=('Ariel', 25)).pack()
+current_chr = "X"
+  
+play_area = tk.Frame(root, width=300, height=300, bg='white')
+X_points = []
+O_points = []
+class XOPoint:
+    def __init__(self, x, y):
+        self.x = x  
+        self.y = y  
+        self.value = None  
+        self.button = tk.Button(play_area, text="", width=10, height=5, command=self.set)
+        self.button.grid(row=x, column=y)
 
-def checkwin(xstate,ystate):
-    wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,7],[2,4,6]]
-    for win in wins:
-        if(sum(xstate[win[0]],xstate[win[1]],xstate[win[2]] == 3)):
-            print('X Won the match')
-            return 1
-        if(sum(ystate[win[0]],ystate[win[1]],ystate[win[2]] == 3)):
-            print('O Won the match')
-            return 0
-    return -1
+    def set(self):
+        global current_chr
+        if not self.value:
+            self.button.configure(text=current_chr, bg='snow', fg='black')
+            self.value = current_chr 
+            if current_chr == "X":
+                X_points.append(self)
+                current_chr = "O"
+            elif current_chr == "O":
+                O_points.append(self)
+                current_chr = "X"
+        check_win()
+  
+    def reset(self):  
+        self.button.configure(text="", bg='white')  
+        self.value = None
+for x in range(1, 4):
+    for y in range(1, 4):
+        XOPoint(x, y)
+class WinningPossibility:
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+    def check(self, for_chr):
+        p1_satisfied = False
+        p2_satisfied = False
+        p3_satisfied = False
+        if for_chr == 'X':
+            for point in X_points:
+                if point.x == self.x1 and point.y == self.y1:
+                    p1_satisfied = True
+                elif point.x == self.x2 and point.y == self.y2:
+                    p2_satisfied = True
+                elif point.x == self.x3 and point.y == self.y3:
+                    p3_satisfied = True
+        elif for_chr == 'O':
+            for point in O_points:
+                if point.x == self.x1 and point.y == self.y1:
+                    p1_satisfied = True
+                elif point.x == self.x2 and point.y == self.y2:
+                    p2_satisfied = True
+                elif point.x == self.x3 and point.y == self.y3:
+                    p3_satisfied = True
+        return all([p1_satisfied, p2_satisfied, p3_satisfied])
+winning_possibilities = [
+    WinningPossibility(1, 1, 1, 2, 1, 3),
+    WinningPossibility(2, 1, 2, 2, 2, 3),
+    WinningPossibility(3, 1, 3, 2, 3, 3),
+    WinningPossibility(1, 1, 2, 1, 3, 1),
+    WinningPossibility(1, 2, 2, 2, 3, 2),
+    WinningPossibility(1, 3, 2, 3, 3, 3),
+    WinningPossibility(1, 1, 2, 2, 3, 3),
+    WinningPossibility(3, 1, 2, 2, 1, 3)
+]
+def check_win():
+    for possibility in winning_possibilities:
+        if possibility.check('X'):
+            print("X won!")
+            return
+        elif possibility.check('O'):
+            print("O won!")
+            return
+play_area.pack(pady=10, padx=10)  
 
-def sum(a,b,c):
-    return a + b + c
-        
-if __name__ == '__main__':
-    xstate = [0,0,0,0,0,0,0,0,0]
-    ystate = [0,0,0,0,0,0,0,0,0] 
-    # 1 for X and 0 for O 
-    turn = 1 
-    print('Welcome to Tic Tac Toe')
-    while(True):
-        printboard(xstate,ystate)
-        if turn == 1:
-            print("X's turn")
-            value = int(input('Enter a value : '))
-            xstate[value] = 1
-        else:
-            print("O's turn")
-            value = int(input('Enter a value : '))
-            ystate[value] = 1
-        cwin=checkwin(xstate,ystate)
-        if(cwin!=-1):
-            print('Match over')
-            break
-        
-        turn = 1 - turn
+if len(X_points) + len(O_points) == 9:
+        print("Draw!")
 
-    
+root.mainloop()
